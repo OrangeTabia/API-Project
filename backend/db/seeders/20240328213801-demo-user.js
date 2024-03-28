7d3a204eb92f328ca472045607d290f6ee11d819
Tabia Ye <tabiaye@gmail.com>
2024-03-28 14:48:21 -0700

    Add Users table migration and seeds

---
+++ backend/db/seeders/20240328213801-demo-user.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require('bcryptjs'); 
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.bear_bnb_schema
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'demo1@gmail.com',
+        username: 'DemoUser1',
+        hashedPassword: bcrypt.hashSync('password1')
+      }, 
+      {
+        email: 'demo2@gmail.com',
+        username: 'DemoUser2',
+        hashedPassword: bcrypt.hashSync('password2')
+      }, 
+      {
+        email: 'demo3@gmail.com',
+        username: 'DemoUser3',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], { validate: true }); 
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users'; 
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['DemoUser1', 'DemoUser2', 'DemoUser3'] }
+    }, {}); 
+  }
+};
