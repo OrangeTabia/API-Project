645fcb54c97313877df5d468cf00e7c455f3fd17
Tabia Ye <tabiaye@gmail.com>
2024-04-02 22:16:21 -0700

    Add get spot details by id route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -124,7 +124,70 @@ router.get('/current', requireAuth, async (req, res) => {
     }); 
 }); 
 
+// Get details of spot by spotId
+router.get('/:spotId', async (req, res) => {
+    const spotId = req.params.spotId;
+    const spot = await Spot.findByPk(spotId, { 
+        include: [
+            {model: SpotImage, attributes: ['id', 'url', 'preview']},
+            {model: User, attributes: ['id', 'firstName', 'lastName']},
+        ]
+    }); 
 
+    if (spot) {
+        // Get the avgRating and numReviews
+
+        const {id, ownerId, address, city, state, country, lat, lng, name, description, price, createdAt, updatedAt } = spot;
+
+        const spotRating = await Spot.findOne({
+            where: {
+                id: spot.id,
+            },
+            attributes: {
+                include: [
+                    [
+                        Sequelize.fn("AVG", Sequelize.col("Reviews.stars")), 
+                        "avgRating"
+                    ],
+                    [
+                        Sequelize.fn("COUNT", Sequelize.col("Reviews.stars")), 
+                        "numReviews"
+                    ]
+                ],
+            },
+            include: [
+                {model: Review, attributes: ['stars']}, 
+            ]
+        });
+
+        const payload = {
+            id, 
+            ownerId,
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price,
+            createdAt,
+            updatedAt,
+            numReviews: spotRating.dataValues.numReviews,
+            avgStarRating: Math.floor(spotRating.dataValues.avgRating * 2) / 2,
+            SpotImages: spot.SpotImages,
+            Owner: spot.User
+        }
+        res.json(payload); 
+    } else {
+        res.json({
+            message: "Spot couldn't be found"
+        }); 
+    }
+
+
+});
 
 
 module.exports = router;
