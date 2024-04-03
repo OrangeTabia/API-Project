8381b56d1854ba6cd64e7bd7c7c5dd31e6a03579
Tabia Ye <tabiaye@gmail.com>
2024-04-03 10:44:49 -0700

    Add create spot route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,6 +3,8 @@ const { Sequelize } = require('sequelize');
 const { Spot, User, Review, SpotImage } = require('../../db/models'); 
 const { Op } = require('sequelize'); 
 const { requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
 
@@ -185,9 +187,68 @@ router.get('/:spotId', async (req, res) => {
             message: "Spot couldn't be found"
         }); 
     }
-
-
 });
+
+
+const validateCreateSpot = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .withMessage('State is required'),
+    check('country')
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required'),
+    check('lat')
+      .exists({ checkFalsy: true })
+      .isFloat({ min: -90, max: 90 })
+      .withMessage('Latitude must be within -90 and 90'),
+    check('lng')
+      .exists({ checkFalsy: true })
+      .isFloat({ min: -180, max: 180 })
+      .withMessage('Latitude must be within -180 and 180'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .isLength({ max: 50 })
+      .withMessage('Name must be less than 50 characters'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .withMessage('Description is required'), 
+    check('price')
+      .exists({ checkFalsy: true })
+      .isInt({ min: 0})
+      .withMessage('Price per day must be a positive number'),
+    handleValidationErrors
+  ];
+
+
+// Create a spot
+router.post('/', [requireAuth, validateCreateSpot], async (req, res) => {
+    const {address, city, state, country, lat, lng, name, description, price} = req.body; 
+
+    // Grab the user from the request 
+    const { user } = req;
+
+    const newSpot = await Spot.create({
+        ownerId: user.id,
+        address,
+        city,
+        state,
+        country,
+        lat,
+        lng,
+        name,
+        description,
+        price
+    }); 
+
+    res.status(201);
+    res.json(newSpot); 
+}); 
 
 
 module.exports = router;
