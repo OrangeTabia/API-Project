35237a3d3c00ee3eeb522a70ffe550239df9d0b5
Tabia Ye <tabiaye@gmail.com>
2024-04-19 11:29:07 -0700

    Add log in as demo user

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -24,6 +24,19 @@ function LoginFormModal() {
       });
   };
 
+  const handleDemoUser = (e) => {
+    e.preventDefault();
+    setErrors({});
+    return dispatch(sessionActions.login({ credential: "demo1@gmail.com", password: "password1" }))
+      .then(closeModal)
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) {
+          setErrors(data.errors);
+        }
+      });
+  };
+
   return (
     <>
       <h1>Log In</h1>
@@ -49,6 +62,8 @@ function LoginFormModal() {
         {errors.credential && <p>{errors.credential}</p>}
         <button type="submit">Log In</button>
       </form>
+      <h2>Log in as Demo User</h2>
+      <button type="submit" onClick={handleDemoUser}>Log in as Demo User</button>
     </>
   );
 }
